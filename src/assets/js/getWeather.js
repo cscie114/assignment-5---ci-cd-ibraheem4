bf2e8bd2507e9bcf47592527467f70d3d6147a83
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-04-25 21:56:17 -0400

    fix: fix weather

--- src/assets/js/getWeather.js
+++ src/assets/js/getWeather.js
@@ -1,16 +1,23 @@
 async function getWeather(location) {
   try {
-    const response = await axios.get(`/api/weather?location=${location}`);
-    const data = response.data;
+    const location = "New York"; // Or use any other location you want
+    const response = await axios.get(
+      `/.netlify/functions/weather?location=${location}`
+    );
+    const weatherData = response.data;
 
-    // Convert temperature from Kelvin to Celsius
-    const temperature = data.main.temp - 273.15;
+    // Extract relevant weather information
+    const temperature = Math.round(weatherData.main.temp - 273.15); // Convert Kelvin to Celsius
+    const description = weatherData.weather[0].description;
+    const cityName = weatherData.name;
+    const countryCode = weatherData.sys.country;
 
-    // Update the DOM with the weather data
-    const weatherElement = document.getElementById("weather-data");
-    weatherElement.textContent = `Temperature in ${
-      data.name
-    }: ${temperature.toFixed(1)}°C`;
+    // Display weather information on the page
+    const weatherContainer = document.getElementById("weather-data");
+    weatherContainer.innerHTML = `
+      <h2>Weather in ${cityName}, ${countryCode}</h2>
+      <p>${description}, ${temperature}°C</p>
+    `;
   } catch (error) {
     console.error("Error fetching weather data:", error);
   }
